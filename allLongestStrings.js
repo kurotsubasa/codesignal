34fc2aab8ec48669f8d06bdaae1c4374480fca0a
kurotsubasa <kurotsubasa10@gmail.com>
2021-06-24 18:18:01 -0400

    Added allLongestStrings challenge file
    Added allLongestStrings challenge file and solution

---
+++ allLongestStrings.js
@@ -0,0 +1,15 @@
+function allLongestStrings(inputArray) {
+    let maxStrings = []
+    let largestString = 0
+
+    for(let i = 0; i < inputArray.length; i++) {
+        const wordArr = inputArray[i].split('')
+        if(wordArr.length > largestString) {
+            largestString = wordArr.length
+            maxStrings = [inputArray[i]]
+        } else if(wordArr.length === largestString) {
+            maxStrings.push(inputArray[i])
+        }
+    }
+    return maxStrings
+}
