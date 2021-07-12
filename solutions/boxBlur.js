752c7f1a44a325317ec0082c70bb2b7f7d113996
kurotsubasa <kurotsubasa10@gmail.com>
2021-07-12 15:14:41 -0400

    Added complete solution for boxBlur
    Added complete solution to boxBlur
    Added boxBlur prompt to README

--- solutions/boxBlur.js
+++ solutions/boxBlur.js
@@ -3,13 +3,17 @@
 // The pixels in the input image are represented as integers. The algorithm distorts the input image in the following way: Every pixel x in the output image has a value equal to the average value of the pixel values from the 3 × 3 square that has its center at x, including x itself. All the pixels on the border of x are then removed.
 //
 // Return the blurred image as an integer, with the fractions rounded down.
-
 function boxBlur(image) {
     const allPixels = []
-    for(let i = 1; i < image.length - 1, i++) {
+    for(let i = 1; i < image.length - 1; i++) {
         const rowPixels = []
         for(let j = 1; j < image[i].length - 1; j++) {
-            let pixelValue = image[i - 1][j - 1] + image[i - 1][j] + image[i - 1][j + 1] + image[i][j - 1] + image[i][j + 1] + image[i + 1][j - 1] + image[i + 1][j] + image[i + 1][j + 1]
+            let pixelValue = image[i-1][j-1] + image[i-1][j] + image[i-1][j+1] + image[i][j-1] + image[i][j] + image[i][j+1] + image[i+1][j-1] + image[i+1][j] + image[i+1][j+1]
+            const avg = (pixelValue / 9)
+            const roundedAvg = Math.floor(avg)
+            rowPixels.push(roundedAvg)
         }
+        allPixels.push(rowPixels)
     }
+    return allPixels
 }
