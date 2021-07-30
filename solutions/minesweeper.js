4c8ca72cb6c2af9325db9507cdb207739362befd
kurotsubasa <kurotsubasa10@gmail.com>
2021-07-29 21:32:25 -0400

    Added logic for adding values
    Added logic for adding values to final array

--- solutions/minesweeper.js
+++ solutions/minesweeper.js
@@ -10,8 +10,28 @@ function minesweeper(matrix) {
     for(let i = 0; i < matrix.length; i++) {
         for(let j = 0; j < matrix[i].length; j++) {
             if(matrix[i][j]) {
-                if(!((i-1) < 0) || (!((j-1) < 0))) {
-                    numMatrix[i-1][j-1] +=1
+                if(!((i-1) < 0)) {
+                    if(!((j-1) < 0)) {
+                        numMatrix[i-1][j-1] +=1
+                        numMatrix[i][j-1] +=1
+                        if((i+1) < matrix.length) {
+                            numMatrix[i+1][j-1] +=1
+                        }
+                    }
+                    if(!(j > matrix[i].length)) {
+                        numMatrix[i-1][j] +=1
+                        numMatrix[i][j] +=1
+                        if((i+1) < matrix.length) {
+                            numMatrix[i+1][j] +=1
+                        }
+                    }
+                    if(!((j+1) > matrix[i].length)) {
+                        numMatrix[i-1][j+1] +=1
+                        numMatrix[i][j+1] +=1
+                        if((i+1) < matrix.length) {
+                            numMatrix[i+1][j+1] +=1
+                        }
+                    }
                 }
             }
         }
